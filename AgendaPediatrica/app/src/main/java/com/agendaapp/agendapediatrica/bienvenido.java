a45b3f42d9b174a19d294bb95d4b1182dd37cd32
Rocio89 <rocio.pastor89@gmail.com>
2017-03-03 14:42:33 -0500

    Para probar. No es con cuenta google

---
+++ AgendaPediatrica/app/src/main/java/com/agendaapp/agendapediatrica/bienvenido.java
@@ -0,0 +1,16 @@
+package com.agendaapp.agendapediatrica;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rocio Note on 02/03/2017.
+ */
+
+public class bienvenido extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+    }
+}
---
+++ AgendaPediatrica/app/src/main/java/com/agendaapp/agendapediatrica/login.java
@@ -0,0 +1,33 @@
+package com.agendaapp.agendapediatrica;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final Button boton = (Button) findViewById(R.id.buttonIngresar);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usuario = ((EditText) findViewById(R.id.editTextUsuario)).getText().toString();
+                String password = ((EditText) findViewById(R.id.editTextPassword)).getText().toString();
+                if (usuario.equals("admin")&& password.equals("admin")){
+                    Intent segundoForm = new Intent(login.this,bienvenido.class);
+                    startActivity(segundoForm);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
+}
